3dcb6b4ad67897601cde088611ee58313be6fc30
Julian K. Arni <jkarni@gmail.com>
2016-04-23 01:21:36 +0200

    Rewrite.
        Being lazy without a new full intepretation isn't paying off.

--- servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/src/Servant/QuickCheck/Internal/Predicates.hs
+++ servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/servant-quickcheck/src/Servant/QuickCheck/Internal/Predicates.hs
@@ -1,132 +1,149 @@
--- | This module contains all logic related to constructing or using
--- @Predicates@.
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 module Servant.QuickCheck.Internal.Predicates where
 
-import           Data.Aeson         (ToJSON (toJSON), Value (..))
-import           Data.Proxy         (Proxy (..))
-import           Data.Void
-import           Network.HTTP.Types (statusCode)
-import           Servant.Common.Req (ServantError (..))
-import           Test.QuickCheck
+import Data.Monoid ((<>))
+import GHC.Generics (Generic)
+import Control.Monad
+import Network.HTTP.Client (Request, Response)
 
-
--- | An HList containing predicates (functions of type @a -> Bool@). This
--- datatype is used to represent both filters (what values to discard when
--- generating arguments to test an API) and tests results (what to consider a
--- failing response).
+{-
+-- | @500 Internal Server Error@ should be avoided - it may represent some
+-- issue with the application code, and it moreover gives the client little
+-- indication of how to proceed or what went wrong.
 --
--- For both filters and test results, only the *first* predicate of the
--- appropriate type is used.
---
--- Use 'emptyPredicates', 'addPredicate', 'addLeftPredicate' and
--- 'addRightPredicate' to construct a @Predicates@.
-data Predicates a where
-    HNil :: Predicates '[]
-    HCons :: (a -> Bool) -> Predicates b -> Predicates (a ': b)
-    HConsC :: Constraint a -> Predicates b -> Predicates (Constraint a ': b)
+-- This function checks that the response code is not 500.
+not500 :: Response b -> IO Bool
+not500
+  = ResponsePredicate "not500" _
 
-class HasPredicate a b where
-    getPredicate :: Predicates a -> b -> Bool
-
-instance {-# OVERLAPPING #-} HasPredicate '[] a where
-    getPredicate _ = const True
-
--- TODO: Find some better way of distinguishing how the predicate is being used
-instance {-# OVERLAPPING #-} HasPredicate '[] (Either ServantError a) where
-    getPredicate _ = discard
-
-instance {-# OVERLAPPING #-} HasPredicate (a ': xs) a where
-    getPredicate (HCons a _) = a
-    getPredicate (HConsC _ _) = error "not impossible, but non-sensical"
-
-data Constraint ctx = Constraint
-  { getConstraint :: forall a . (ctx a) => a -> Bool }
-
--- This is a little bit of a hack. Ideally instances would match when the
--- predicate is polymorphic, but that doesn't work since the polymorphic type
--- may have to unify with multiple distict values.
---
--- It may however be possible to define a MPTC from monomorphic to polymorphic
--- datatypes to avoid this issue.
-instance {-# OVERLAPPING #-}
-    HasPredicate (Either ServantError Void ': xs) (Either ServantError a) where
-    getPredicate (HCons f _) x = case x of
-                                    Left e  -> f (Left e)
-                                    Right _ -> True
-
-instance {-# OVERLAPPING #-} (ctx a)
-    => HasPredicate (Constraint ctx ': xs) (Either ServantError a) where
-    getPredicate (HConsC f _) x = case x of
-        Left _  -> discard  -- Not clear whether checking for FailureResponse is better
-        Right v -> getConstraint f v
-    getPredicate (HCons _ _) _  = error "not impossible, but non-sensical"
-
-instance {-# OVERLAPPABLE #-} (ls ~ (b ': xs), HasPredicate xs a)
-    => HasPredicate ls a where
-    getPredicate (HCons _ xs) = getPredicate xs
-    getPredicate _ = error "impossible"
-
--- | Add a predicate to a list of predicates. Note that the predicate may not
--- be polymorphic.
-addPredicate :: (a -> Bool) -> Predicates b -> Predicates (a ': b)
-addPredicate = HCons
-
--- | Add a predicate with a class constraint.
---
--- Note that every possible argument must be an instance of that class for this
--- to typecheck. In other words, if the @Predicates@ is being used for return
--- types, every return type in the API must be an instance of the class. If
--- it's being used for filtering, every capture, header, body, etc. type must
--- be an instance of that class.
---
--- This can be used to for example test that returned JSON has certain
--- properties, or (via generics) that if any datatype contains a (possibly
--- nested) field of a particular type, it always meets certain properties.
-addPolyPredicate :: proxy ctx -> (forall a. ctx a => a -> Bool) -> Predicates b
-  -> Predicates (Constraint ctx ': b)
-addPolyPredicate _ p = HConsC (Constraint p)
-
--- | Given a predicate over an @p :: a -> Bool@, add a predicate to the @Predicates@
--- list that succeeds on an @val :: Either ServantError a@ if @val@ is a
--- @Left@, or a @Right v@ such that @p a == True@.
-addRightPredicate :: (a -> Bool) -> Predicates b -> Predicates (Either ServantError a ': b)
-addRightPredicate p = addPredicate $ either (const True) p
-
--- | The @Left@ analog of 'addRightPredicate'.
-addLeftPredicate :: (ServantError -> Bool) -> Predicates b
-    -> Predicates (Either ServantError Void ': b)
-addLeftPredicate p = addPredicate $ either p (error "impossible")
-
--- | An empty list of predicates. This doesn't discard any values when used as
--- a filter, and doesn't fail any value when used as a condition to satisfy.
-emptyPredicates :: Predicates '[]
-emptyPredicates = HNil
-
--- * Useful predicates
-
--- | A @Predicates@ list that fails a test if the response is an HTTP 500 error.
-never500s :: Predicates '[Either ServantError Void]
-never500s = addLeftPredicate go emptyPredicates
-  where
-   go (FailureResponse x _ _) = statusCode x /= 500
-   go _ = True
-
--- | A @Predicates@ list that fails a test if the response is anything but a
--- top-level object (e.g., if it is an array or literal).
---
--- Returning anything other than object is considered bad practice, as
+-- | Returning anything other than an object when returning JSON is considered
+-- bad practice, as:
 --
 --   (1) it is hard to modify the returned value while maintaining backwards
---   compatibility;
---   (2) many older tools do not support top-level arrays;
+--   compatibility
+--   (2) many older tools do not support top-level arrays
 --   (3) whether top-level numbers, booleans, or strings are valid JSON depends
---   on what RFC you're going by;
---   (4) there are security issues with top-level arrays.
-onlyJsonObjects :: Predicates '[Constraint ToJSON]
-onlyJsonObjects = addPolyPredicate (Proxy :: Proxy ToJSON) go emptyPredicates
-  where
-    go x = case toJSON x of
-      Object _ -> True
-      _        -> False
+--   on what RFC you're going by
+--   (4) there are security issues with top-level arrays
+--
+-- This function checks that any @application/json@ responses only return JSON
+-- objects (and not arrays, strings, numbers, or booleans) at the top level.
+onlyJsonObjects :: Response b -> IO Bool
+onlyJsonObjects
+  = ResponsePredicate "onlyJsonObjects" _
+
+-- | When creating a new resource, it is good practice to provide a @Location@
+-- header with a link to the created resource.
+--
+-- This function checks that every @201 Created@ response contains a @Location@
+-- header, and that the link in it responds with a 2XX response code to @GET@
+-- requests.
+--
+-- References: <RFC 7231, Section 6.3.2 https://tools.ietf.org/html/rfc7231#section-6.3.2>
+createContainsValidLocation :: Response b -> IO Bool
+createContainsValidLocation
+  = ResponsePredicate "createContainsValidLocation" _
+
+getsHaveLastModifiedHeader :: Response b -> IO Bool
+getsHaveLastModifiedHeader
+  = ResponsePredicate "getsHaveLastModifiedHeader" _
+
+-- | When an HTTP request has a method that is not allowed, a 405 response
+-- should be returned. Additionally, it is good practice to return an @Allow@
+-- header with the list of allowed methods.
+--
+-- This function checks that every @405 Method Not Allowed@ response contains
+-- an @Allow@ header with a list of standard HTTP methods.
+notAllowedContainsAllowHeader :: Response b -> IO Bool
+notAllowedContainsAllowHeader
+  = ResponsePredicate "notAllowedContainsAllowHeader" _
+
+-- | When a request contains an @Accept@ header, the server must either return
+-- content in one of the requested representations, or respond with @406 Not
+-- Acceptable@.
+--
+-- This function checks that every *successful* response has a @Content-Type@
+-- header that matches the @Accept@ header.
+honoursAcceptHeader :: Predicate b Bool
+honoursAcceptHeader
+  = RequestPredicate "honoursAcceptHeader" _
+
+-- | Whether or not a representation should be cached, it is good practice to
+-- have a @Cache-Control@ header for @GET@ requests. If the representation
+-- should not be cached, used @Cache-Control: no-cache@.
+--
+-- This function checks that @GET@ responses have a valid @Cache-Control@
+-- header.
+--
+-- References: RFC 7234 Section 5.2
+-- https://tools.ietf.org/html/rfc7234#section-5.2
+getsHaveCacheControlHeader :: Predicate b Bool
+getsHaveCacheControlHeader
+  = ResponsePredicate "getsHaveCacheControlHeader" _
+
+-- | Like 'getsHaveCacheControlHeader', but for @HEAD@ requests.
+headsHaveCacheControlHeader :: Predicate b Bool
+headsHaveCacheControlHeader
+  = ResponsePredicate "headsHaveCacheControlHeader" _
+
+-- |
+--
+-- If the original request modifies the resource, this function makes two
+-- requests:
+--
+--   (1) Once, with the original request and a future date as the
+--   @If-Unmodified-Since@, which is expected to succeed.
+--   (2) Then with the original request again, with a @If-Unmodified-Since@
+--   safely in the past. Since presumably the representation has been changed
+--   recently (by the first request), this is expected to fail with @412
+--   Precondition Failure@.
+--
+-- Note that the heuristic used to guess whether the original request modifies
+-- a resource is simply whether the method is @PUT@ or @PATCH@, which may be
+-- incorrect in certain circumstances.
+supportsIfUnmodifiedSince :: Predicate b Bool
+supportsIfUnmodifiedSince
+  = ResponsePredicate "supportsIfUnmodifiedSince" _
+
+-- | @OPTIONS@ responses should contain an @Allow@ header with the list of
+-- allowed methods.
+--
+-- If a request is an @OPTIONS@ request, and if the response is a successful
+-- one, this function checks the response for an @Allow@ header. It fails if:
+--
+--   (1) There is no @Allow@ header
+--   (2) The @Allow@ header does not have standard HTTP methods in the correct
+--   format
+--   (3) Making a request to the same URL with one of those methods results in
+--   a 404 or 405.
+optionsContainsValidAllow :: Predicate b Bool
+optionsContainsValidAllow
+  = ResponsePredicate "optionsContainsValidAllow" _
+
+-- | Link headers are a standardized way of presenting links that may be
+-- relevant to a client.
+--
+-- This function checks that any @Link@ headers have values in the correct
+-- format.
+--
+-- References: RFC 5988 Section 5
+-- https://tools.ietf.org/html/rfc5988
+linkHeadersAreValid :: Predicate b Bool
+linkHeadersAreValid
+  = ResponsePredicate "linkHeadersAreValid" _
+
+-- | Any @401 Unauthorized@ response must include a @WWW-Authenticate@ header.
+--
+-- This function checks that, if a response has status code 401, it contains a
+-- @WWW-Authenticate@ header.
+--
+-- References: RFC 7235 Section 4.1
+-- https://tools.ietf.org/html/rfc7235#section-4.1
+unauthorizedContainsWWWAuthenticate :: Predicate b Bool
+unauthorizedContainsWWWAuthenticate
+  = ResponsePredicate "unauthorizedContainsWWWAuthenticate" _
+-}
+
+data Predicate b r
+  = ResponsePredicate String (Response b -> IO r)
+  | RequestPredicate String (Request -> [Response b -> IO r] -> IO r)
+  deriving (Generic)
