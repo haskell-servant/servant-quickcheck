9e96ea7b87e61c00038a3de2abfa63c22259e7d1
Julian K. Arni <jkarni@gmail.com>
2016-10-12 13:14:51 +0200

    Unbias generation of requests.
        So that each endpoint is picked with the same frequency.

--- src/Servant/QuickCheck/Internal/HasGenRequest.hs
+++ src/Servant/QuickCheck/Internal/HasGenRequest.hs
@@ -14,101 +14,107 @@ import Prelude.Compat
 import Servant
 import Servant.API.ContentTypes (AllMimeRender (..))
 import Servant.Client           (BaseUrl (..), Scheme (..))
-import Test.QuickCheck          (Arbitrary (..), Gen, elements, oneof)
+import Test.QuickCheck          (Arbitrary (..), Gen, elements, frequency)
 #if MIN_VERSION_servant(0,8,0)
 import qualified Data.ByteString as BS
 #endif
 
+runGenRequest :: HasGenRequest a => Proxy a -> Gen (BaseUrl -> Request)
+runGenRequest = snd . genRequest
+
 
 class HasGenRequest a where
-    genRequest :: Proxy a -> Gen (BaseUrl -> Request)
+                        --  frequency   generator
+                        --    vvv         vvv
+    genRequest :: Proxy a -> (Int, Gen (BaseUrl -> Request))
 
 instance (HasGenRequest a, HasGenRequest b) => HasGenRequest (a :<|> b) where
     genRequest _
-      = oneof [ genRequest (Proxy :: Proxy a)
-              , genRequest (Proxy :: Proxy b)
-              ]
+      = (lf + rf, frequency [l, r])
+      where
+        l@(lf, _) = genRequest (Proxy :: Proxy a)
+        r@(rf, _) = genRequest (Proxy :: Proxy a)
 
 instance (KnownSymbol path, HasGenRequest b) => HasGenRequest (path :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       old' <- old
-      return $ \burl -> let r = old' burl in r { path = new <> path r }
+      return $ \burl -> let r = old' burl in r { path = new <> path r })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         new = cs $ symbolVal (Proxy :: Proxy path)
 
 instance (Arbitrary c, HasGenRequest b, ToHttpApiData c )
     => HasGenRequest (Capture x c :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       old' <- old
       new' <- toUrlPiece <$> new
-      return $ \burl -> let r = old' burl in r { path = cs new' <> path r }
+      return $ \burl -> let r = old' burl in r { path = cs new' <> path r })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         new = arbitrary :: Gen c
 
 #if MIN_VERSION_servant(0,8,0)
 instance (Arbitrary c, HasGenRequest b, ToHttpApiData c )
     => HasGenRequest (CaptureAll x c :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       old' <- old
       new' <- fmap (cs . toUrlPiece) <$> new
       let new'' = BS.intercalate "/" new'
-      return $ \burl -> let r = old' burl in r { path = new'' <> path r }
+      return $ \burl -> let r = old' burl in r { path = new'' <> path r })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         new = arbitrary :: Gen [c]
 #endif
 
 instance (Arbitrary c, KnownSymbol h, HasGenRequest b, ToHttpApiData c)
     => HasGenRequest (Header h c :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       old' <- old
       new' <- toUrlPiece <$> new
       return $ \burl -> let r = old' burl in r {
-          requestHeaders = (hdr, cs new') : requestHeaders r }
+          requestHeaders = (hdr, cs new') : requestHeaders r })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         hdr = fromString $ symbolVal (Proxy :: Proxy h)
         new = arbitrary :: Gen c
 
 instance (AllMimeRender x c, Arbitrary c, HasGenRequest b)
     => HasGenRequest (ReqBody x c :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       old' <- old
       new' <- new
       (ct, bd) <- elements $ allMimeRender (Proxy :: Proxy x) new'
       return $ \burl -> let r = old' burl in r {
           requestBody = RequestBodyLBS bd
         , requestHeaders = ("Content-Type", renderHeader ct) : requestHeaders r
-        }
+        })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         new = arbitrary :: Gen c
 
 instance (KnownSymbol x, Arbitrary c, ToHttpApiData c, HasGenRequest b)
     => HasGenRequest (QueryParam x c :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       new' <- new
       old' <- old
       return $ \burl -> let r = old' burl in r {
           queryString = queryString r
-                     <> param <> "=" <> cs (toQueryParam new') }
+                     <> param <> "=" <> cs (toQueryParam new') })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         param = cs $ symbolVal (Proxy :: Proxy x)
         new = arbitrary :: Gen c
 
 instance (KnownSymbol x, Arbitrary c, ToHttpApiData c, HasGenRequest b)
     => HasGenRequest (QueryParams x c :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       new' <- new
       old' <- old
       return $ \burl -> let r = old' burl in r {
           queryString = queryString r
-                     <> if length new' > 0 then fold (toParam <$> new') else ""}
+                     <> if length new' > 0 then fold (toParam <$> new') else ""})
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         param = cs $ symbolVal (Proxy :: Proxy x)
         new = arbitrary :: Gen [c]
         toParam c = param <> "[]=" <> cs (toQueryParam c)
@@ -116,22 +122,22 @@ instance (KnownSymbol x, Arbitrary c, ToHttpApiData c, HasGenRequest b)
 
 instance (KnownSymbol x, HasGenRequest b)
     => HasGenRequest (QueryFlag x :> b) where
-    genRequest _ = do
+    genRequest _ = (oldf, do
       old' <- old
       return $ \burl -> let r = old' burl in r {
-          queryString = queryString r <> param <> "=" }
+          queryString = queryString r <> param <> "=" })
       where
-        old = genRequest (Proxy :: Proxy b)
+        (oldf, old) = genRequest (Proxy :: Proxy b)
         param = cs $ symbolVal (Proxy :: Proxy x)
 
 instance (ReflectMethod method)
     => HasGenRequest (Verb (method :: k) (status :: Nat) (cts :: [*]) a) where
-    genRequest _ = return $ \burl -> defaultRequest
+    genRequest _ = (1, return $ \burl -> defaultRequest
        { host = cs $ baseUrlHost burl
        , port = baseUrlPort burl
        , secure = baseUrlScheme burl == Https
        , method = reflectMethod (Proxy :: Proxy method)
-       }
+       })
 
 instance (HasGenRequest a) => HasGenRequest (RemoteHost :> a) where
     genRequest _ = genRequest (Proxy :: Proxy a)
