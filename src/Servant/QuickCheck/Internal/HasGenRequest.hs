60c8239a6f5d72c65574f7202695c2f552dc9a9e
Michal J. Gajda <mjgajda@migamake.com>
2021-06-16 18:48:25 +0200

    Update to recent LTS, GHC 8.10, and servant 0.18.

--- src/Servant/QuickCheck/Internal/HasGenRequest.hs
+++ src/Servant/QuickCheck/Internal/HasGenRequest.hs
@@ -82,6 +82,16 @@ instance (Arbitrary c, HasGenRequest b, ToHttpApiData c )
         (oldf, old) = genRequest (Proxy :: Proxy b)
         new = arbitrary :: Gen c
 
+instance (Arbitrary a, HasGenRequest b, ToHttpApiData a)
+    => HasGenRequest (Fragment a :> b) where
+    genRequest _ = (oldf, do
+      old' <- old
+      new' <- toUrlPiece <$> new
+      return $ \burl -> let r = old' burl in r { path = path r <> cs new' })
+      where
+        (oldf, old) = genRequest (Proxy :: Proxy b)
+        new = arbitrary :: Gen a
+      
 instance (Arbitrary c, HasGenRequest b, ToHttpApiData c )
     => HasGenRequest (CaptureAll x c :> b) where
     genRequest _ = (oldf, do
@@ -195,3 +205,4 @@ instance (HasGenRequest a) => HasGenRequest (WithNamedContext x y a) where
 -- TODO: Try logging in
 instance (HasGenRequest a) => HasGenRequest (BasicAuth x y :> a) where
     genRequest _ = genRequest (Proxy :: Proxy a)
+
